ba98f5164bece346595482d8779bf061aed4085b
Eymard Neto <eymardneto115@gmail.com>
2021-01-05 22:37:51 -0300

    Webpack configurado

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path'); // Trabalhar com caminhos node
+module.exports ={
+    entry: path.resolve(__dirname, 'src', 'index.js'), // Arquivo de entrada
+
+    devServer: {
+        contentBase: './public',
+    },
+
+    output:{ // Arquivo de saída
+        path: path.resolve(__dirname, 'public'), 
+        filename: 'bundle.js'
+    },
+
+
+    module: { //Module configs 
+        /**
+         * Rules of readed js files 
+         * 
+         * babel-loader -> Javascript
+         */
+        rules: [ 
+            {
+                test: /\.js$/, // Execute only that .js extension 
+                exclude: /node_modules/, //Quando trazer arquivos js do node modules, não passar eles pelo babel
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
+}
